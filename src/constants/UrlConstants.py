41ac4d9c8ed75e07832705884db7e200ac055ee5
Anshul Hajare <anshulhajare09@gmail.com>
2020-05-26 17:09:40 +0530

    Create UrlConstants.py
    Common Constants in a single file

---
+++ src/constants/UrlConstants.py
@@ -0,0 +1,3 @@
+AUTH_TOKEN_URL = "https://public-apis-api.herokuapp.com/api/v1/auth/token"
+CATEGORY_FETCH_URL = "https://public-apis-api.herokuapp.com/api/v1/apis/categories?page="
+ENTRY_FETCH_URL = "https://public-apis-api.herokuapp.com/api/v1/apis/entry?{}"
