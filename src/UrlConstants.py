7541995286fb7e50e646bc34fe5b3e9a4e301317
Anshul Hajare <anshulhajare09@gmail.com>
2020-05-26 18:58:53 +0530

    Delete UrlConstants.py
    Reverting past commit

--- src/UrlConstants.py
+++
@@ -1,4 +0,0 @@
-# URL constants for api
-AUTH_TOKEN_URL = "https://public-apis-api.herokuapp.com/api/v1/auth/token"
-CATEGORY_FETCH_URL = "https://public-apis-api.herokuapp.com/api/v1/apis/categories?page="
-ENTRY_FETCH_URL = "https://public-apis-api.herokuapp.com/api/v1/apis/entry?{}"
