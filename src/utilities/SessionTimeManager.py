3a0552201b93cfe451bf9b2bae46728cbd5afb29
Anshul Hajare <anshulhajare09@gmail.com>
2020-05-26 19:19:26 +0530

    Update SessionTimeManager.py
    Added doc

--- src/utilities/SessionTimeManager.py
+++ src/utilities/SessionTimeManager.py
@@ -1,11 +1,14 @@
 import asyncio
 from datetime import datetime, date, time, timedelta
 
+# Rate Limit currently is 60 seconds
+# Makes the system sleep for specified duration
 async def sleepTimeout(sleep):
     await asyncio.sleep(sleep)
     return datetime.now().timestamp()
 
-
+# Compares two time stamps 
+# Checks whether token has expired or not
 async def checkTokenExpiration(currTimeStamp ,tokenTimeStamp):
     if(currTimeStamp - tokenTimeStamp > 5):
         return True
